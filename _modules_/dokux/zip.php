87e139ac6325b5fc1b8ff1c122df59c442c3fcd1
Nailuj2k <soporte@extralab.net>
2026-09-05 19:40:55 +0200

    Remove deprecated  module from ExtFW repository

--- _modules_/dokux/zip.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    // This file is included in control_panel/ajax.php when creating a zip file for the module. 
-    // It should contain calls to addToZip() for any additional files that need to be included in the zip file.
-
-    addToZip($hzip,SCRIPT_DIR_LIB.'/pdf.js');
-    addToZip($hzip,SCRIPT_DIR_LIB.'/jquery/jquery.filedrop.js');    
-    addToZip($hzip,SCRIPT_DIR_LIB.'/getID3');
